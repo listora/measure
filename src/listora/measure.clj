10cbc205ada505a2555d8df5bca11c3df8a57a89
James Reeves <jreeves@weavejester.com>
2014-03-28 20:32:00 +0000

    Add measure function and profile macro

--- src/listora/measure.clj
+++ src/listora/measure.clj
@@ -1,6 +1,24 @@
-(ns listora.measure)
+(ns listora.measure
+  (:require [clojure.core.async :as a :refer [>!!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def measurements
+  "A channel that receives measurements."
+  (a/chan (a/buffer 255)))
+
+(defn measure
+  "Add a measurement to the measurements channel."
+  [key value]
+  {:pre [(keyword? key)]}
+  (>!! measurements [key value]))
+
+(defn profile* [key func]
+  (let [t0  (System/nanoTime)
+        ret (func)
+        t1  (System/nanoTime)]
+    (measure key (/ (- t1 t0) 1e9))
+    ret))
+
+(defmacro profile
+  "Evaluate the body and take a measurement of the time it takes to complete."
+  [key & body]
+  `(profile* ~key (^:once fn* [] ~@body)))
