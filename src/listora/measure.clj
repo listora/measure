0f4893a09a2c3c7b0c37d5d07e37681e7a3f71ba
James Reeves <jreeves@weavejester.com>
2014-03-28 23:34:38 +0000

    Add 'imply' macro

--- src/listora/measure.clj
+++ src/listora/measure.clj
@@ -5,21 +5,29 @@
   "A channel that receives measurements."
   (a/chan (a/buffer 255)))
 
+(def ^:dynamic *measurement* {})
+
 (defn measure
-  "Add a measurement to the measurements channel."
-  [key value]
-  {:pre [(keyword? key)]}
-  (>!! measurements [key value]))
+  "Add a measurement map to the measurements channel."
+  [measurement]
+  (>!! measurements (merge *measurement* measurement)))
+
+(defmacro imply
+  "Merges a map with any measurement taken in the body."
+  [measurement & body]
+  `(binding [*measurement* (merge *measurement* ~measurement)]
+     ~@body))
 
 (defn profile* [key func]
   (let [t0  (System/nanoTime)
         ret (func)
         t1  (System/nanoTime)]
-    (measure key (/ (- t1 t0) 1e9))
+    (measure {key (/ (- t1 t0) 1e9)})
     ret))
 
 (defmacro profile
-  "Evaluate the body and take a measurement of the time it takes to complete."
+  "Evaluate the body and take a measurement of the time it takes to complete
+  in seconds."
   [key & body]
   `(profile* ~key (^:once fn* [] ~@body)))
 
