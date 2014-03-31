b0566eeb869254e34d28f211824269a90268d96a
James Reeves <jreeves@weavejester.com>
2014-03-31 14:21:45 +0100

    Release 0.1.0

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ from running server applications.
 Add the following dependency to your project.clj file:
 
 ```clojure
-[listora/measure "0.1.0-SNAPSHOT"]
+[listora/measure "0.1.0"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/measure "0.1.0-SNAPSHOT"
+(defproject listora/measure "0.1.0"
   :description "A profiling and metrics library"
   :url "https://github.com/listora/measure"
   :license {:name "Eclipse Public License"
