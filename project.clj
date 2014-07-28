85c597836f8717d80c5d1ff168ee3d159c1f4068
Roman Scherer <roman@burningswell.com>
2014-07-28 18:50:57 +0200

    Add optional key argument to add-profiling!.
    This adds the option to profile a fn via a different logging key, and
    not the default namespace.fn-name key.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/measure "0.1.2"
+(defproject listora/measure "0.1.3-SNAPSHOT"
   :description "A profiling and metrics library"
   :url "https://github.com/listora/measure"
   :license {:name "Eclipse Public License"
