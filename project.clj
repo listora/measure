6511c677a20b12ef7a1dda773cd08ea45e1ab837
Roman Scherer <roman@burningswell.com>
2014-07-29 13:05:29 +0200

    Add :deploy-repositories to project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/listora/measure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]])
