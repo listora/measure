acc526c4b1b942c40a361332a7055ec7f1bd550d
Roman Scherer <roman@burningswell.com>
2015-01-15 15:26:37 +0100

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/measure "0.1.4"
+(defproject listora/measure "0.1.5-SNAPSHOT"
   :description "A profiling and metrics library"
   :url "https://github.com/listora/measure"
   :license {:name "Eclipse Public License"
